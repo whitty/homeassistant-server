feb2fc80c9f6cd3a08367eda267514ea5a3fdefd
Greg Whiteley <greg.whiteley@gmail.com>
2026-05-05 22:40:30 +1000

    and the rest of the artists

--- music_assistant/providers/ytmusic/helpers.py
+++ music_assistant/providers/ytmusic/helpers.py
@@ -170,6 +170,14 @@ async def get_library_artists(
             del artist["browseId"]
             del artist["artist"]
 
+        library_artists = ytm.get_library_artists(limit=9999)
+        logging.getLogger("music_assistant").getChild("YouTube Music").error("GW: library_artists=%s", repr(library_artists))
+        for artist in library_artists:
+            artist["id"] = artist["browseId"]
+            artist["name"] = artist["artist"]
+            del artist["browseId"]
+            del artist["artist"]
+
         upload_artists = ytm.get_library_upload_artists(limit=9999)
         logging.getLogger("music_assistant").getChild("YouTube Music").error("GW: upload_artists=%s", repr(upload_artists))
         for artist in upload_artists:
@@ -178,7 +186,7 @@ async def get_library_artists(
             del artist["browseId"]
             del artist["artist"]
 
-        return artists + upload_artists
+        return artists + library_artists + upload_artists
 
     return await asyncio.to_thread(_get_library_artists)
 
